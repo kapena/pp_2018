8c3dceceb6b7dcced8a169d86027d09335818e32
Brent <brentw.white@gmail.com>
2018-02-22 22:12:32 -1000

    adding fine tweaks to layouts and functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+$('.pp-nav').on('sticky.zf.stuckto:top', function(){
+  $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top',function(){
+  $(this).removeClass('shrink');
+});
+
+$('.pp-nav, .nav-icons, .s-link').on('sticky.zf.stuckto:top', function(){
+    $('.nav-icons').addClass('hideIcons');
+    $('.s-link').addClass('sticky-links');
+}).on('sticky.zf.unstuckfrom:top',function(){
+  $('.nav-icons').removeClass('hideIcons');
+  $('.s-link').removeClass('sticky-links');
+});
