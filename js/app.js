99814f694078d53ac8a36c68a6d83fcc8c6d5583
Brent <brentw.white@gmail.com>
2018-03-06 20:19:19 -1000

    point plumbing build update all pages complete

--- js/app.js
+++ js/app.js
@@ -4,11 +4,17 @@ $('.pp-nav').on('sticky.zf.stuckto:top', function(){
 }).on('sticky.zf.unstuckfrom:top',function(){
   $(this).removeClass('shrink');
 });
-
-$('.pp-nav, .nav-icons, .s-link').on('sticky.zf.stuckto:top', function(){
+// Sticky nav additional feature
+$('.pp-nav, .nav-icons, .s-link, .pplogo').on('sticky.zf.stuckto:top', function(){
+    $('.pplogo').addClass('smaller');
     $('.nav-icons').addClass('hideIcons');
     $('.s-link').addClass('sticky-links');
 }).on('sticky.zf.unstuckfrom:top',function(){
+  $('.pplogo').removeClass('smaller');
   $('.nav-icons').removeClass('hideIcons');
   $('.s-link').removeClass('sticky-links');
+});
+// add fade out on close
+$('.off-canvas a').on('click', function() {
+    $('.off-canvas').foundation('close');
 });
